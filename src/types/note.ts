507f085bdd12431156d76d632c9728abeb431651
AnnaYarmolenko <93723763+AnnaYarmolenko@users.noreply.github.com>
2025-08-24 15:21:55 +0300

    Added HW files

---
+++ src/types/note.ts
@@ -0,0 +1,14 @@
+export type NoteTag = "Work" | "Personal" | "Meeting" | "Shopping" | "Todo";
+
+export interface Note {
+  id: string;
+  title: string;
+  content: string;
+  tag: NoteTag;
+}
+
+export interface NoteFormValues {
+  title: string;
+  content: string;
+  tag: NoteTag;
+}
